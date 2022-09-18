05a7733ac336d4912440ac3f5959534762e72bcc
Saniya Shaikh <51195476+sanu29@users.noreply.github.com>
2022-09-18 06:52:48 +0000

    chores:PORt made env

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ require("./DB")  //DB connecting
 const router = require("./Routes/main");
 const { json } = require("express");
 app.use('/',router);
-
-var server = app.listen(8080, function() {
+const PORT = process.env.PORT || 8080;
+var server = app.listen(PORT, function() {
   var host = server.address().address
   var port = server.address().port
 
